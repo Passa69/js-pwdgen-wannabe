ad65a4d8143306976632cbd05a95fea00028ed6e
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-20 15:55:08 +0200

    Aggiunta delle variabili js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // Assegnare una password all'utente, generata dai suoi dati
 
 // Chiediamo all'utente il nome
+let name = prompt("Scrivi il tuo nome")
 
 // Chiediamo all'utente il suo cognome
+let surname = prompt("Scrivi il tuo cognome")
 
 // Chiediamo all'utente il suo colore preferito
+let color = prompt("Scrivi il tuo colore preferito")
 
-// Generiamo la password
+// Ricaviamo la password
+let password = 
+
+// Comunichiamo la password all'utente
+document.getElementById["mio_id"].innerHTML = `La password assegnata è: `
