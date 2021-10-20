ff30baec893f83c24974ba1d95c698e538af3870
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-20 16:16:18 +0200

    Conclusione generazione password

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // Assegnare una password all'utente, generata dai suoi dati
 
 // Chiediamo all'utente il nome
-let name = prompt("Scrivi il tuo nome")
+let name = prompt("Scrivi il tuo nome");
 
 // Chiediamo all'utente il suo cognome
-let surname = prompt("Scrivi il tuo cognome")
+let surname = prompt("Scrivi il tuo cognome");
 
 // Chiediamo all'utente il suo colore preferito
-let color = prompt("Scrivi il tuo colore preferito")
+let color = prompt("Scrivi il tuo colore preferito");
 
 // Ricaviamo la password
-let password = 
+// let password = 
 
 // Comunichiamo la password all'utente
-document.getElementById("mio_id").innerHTML = `La password assegnata è: 21`
+document.getElementById("mio_id").innerHTML = `La password assegnata è: ${name}${surname}${color}21`;
