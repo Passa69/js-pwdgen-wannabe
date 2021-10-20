01774a79b7e5879aeb1d9ba49d4e5896751215d4
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-20 15:30:45 +0200

    Descrizione dei passaggi js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Assegnare una password all'utente, generata dai suoi dati
+
+// Chiediamo all'utente il nome
+
+// Chiediamo all'utente il suo cognome
+
+// Chiediamo all'utente il suo colore preferito
+
+// Generiamo la password
