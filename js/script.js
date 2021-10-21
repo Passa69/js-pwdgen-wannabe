d451cf0e676cdbfbb2bf28ce69949a0b5b4f47cf
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-21 09:44:00 +0200

    aggiunto debug

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 // Assegnare una password all'utente, generata dai suoi dati
 
 // Chiediamo all'utente il nome
-let name = prompt("Scrivi il tuo nome");
+let username = prompt("Scrivi il tuo nome");
+console.log(username);
 
 // Chiediamo all'utente il suo cognome
 let surname = prompt("Scrivi il tuo cognome");
+console.log(surname);
 
 // Chiediamo all'utente il suo colore preferito
 let color = prompt("Scrivi il tuo colore preferito");
+console.log(color);
 
 // Ricaviamo la password
 // let password = 
 
 // Comunichiamo la password all'utente
-document.getElementById("mio_id").innerHTML = `La password assegnata è: ${name}${surname}${color}21`;
+document.getElementById("mio_id").innerHTML = `La password assegnata è: ${username}${surname}${color}21`;
